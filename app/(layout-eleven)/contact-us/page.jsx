c563f26e65b503071991aef00c0d62f037c1991a
MbaliPuso <mbalipuso@gmail.com>
2025-05-05 11:38:15 +0200

    update meta data

--- app/(layout-eleven)/contact-us/page.jsx
+++ app/(layout-eleven)/contact-us/page.jsx
@@ -2,8 +2,8 @@ import BreadCrumb from "@/components/common/Breadcrumb";
 import Contact from "@/components/contact-us-page";
 import MyMap from "@/components/contact-us-page/MyMap";
 export const metadata = {
-	title: "Sofax || Responsive Next.js Template Contact Us Page",
-	description: "Sofax || Responsive Next.js Template",
+	title: "Kilomita: Fuel Financing for Mobility Businesses",
+	description: "Empower your fleet with instant fuel financing solutions tailored for ride-hailing platforms, e-commerce delivery services, and independent contractors across Africa.",
 };
 function ContactUs() {
 	return (
--- app/one-page/home-one/layout.jsx
+++ app/one-page/home-one/layout.jsx
@@ -1,8 +1,8 @@
 import Footer from "@/components/home/home-one/footer";
 import Header from "@/components/home/home-one/header/one-page";
 export const metadata = {
-	title: "Sofax || Responsive Next.js Template Home One",
-	description: "Sofax || Responsive Next.js Template",
+	title: "Kilomita: Fuel Financing for Mobility Businesses",
+	description: "Empower your fleet with instant fuel financing solutions tailored for ride-hailing platforms, e-commerce delivery services, and independent contractors across Africa.",
 };
 function OnePageLayoutOne({ children }) {
 	return (
