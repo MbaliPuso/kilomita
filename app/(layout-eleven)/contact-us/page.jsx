2f169aee86867508362ad536cc2b319160672b7a
MbaliPuso <mbalipuso@gmail.com>
2025-05-07 21:36:28 +0200

    update

--- app/(layout-eleven)/contact-us/page.jsx
+++ app/(layout-eleven)/contact-us/page.jsx
@@ -1,6 +1,5 @@
 import BreadCrumb from "@/components/common/Breadcrumb";
 import Contact from "@/components/contact-us-page";
-import MyMap from "@/components/contact-us-page/MyMap";
 export const metadata = {
 	title: "Kilomita: Fuel Financing for Mobility Businesses",
 	description: "Empower your fleet with instant fuel financing solutions tailored for ride-hailing platforms, e-commerce delivery services, and independent contractors across Africa.",
