4554a8167c3a60f69965f5f6fbf0870dd51d7408
MbaliPuso <mbalipuso@gmail.com>
2025-05-26 12:30:26 +0200

    navigation update

--- components/home/home-one/header/one-page/index.jsx
+++ components/home/home-one/header/one-page/index.jsx
@@ -1,7 +1,10 @@
+"use client";
 import MobileNavbar from "@/components/common/navigation/mobile-nav/MobileNavbar";
 import HeaderButton from "../HeaderButton";
 import HeaderLogo from "../HeaderLogo";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const menuItemsData = [
 	{
 		title: "Home",
@@ -21,6 +24,8 @@ const menuItemsData = [
 	},
 ];
 function Header() {
+	const pathname = usePathname();
+	const isContactPage = pathname === "/contact-us";
 	return (
 		<header className="site-header sofax-header-section site-header--menu-center bg-white" id="sticky-menu">
 			<div className="container">
@@ -35,19 +40,19 @@ function Header() {
 									</Link>
 								</li>
 								<li className="nav-item">
-									<a href="#service" className="nav-link-item">
+									<Link href={`${isContactPage ? "/#service" : "#service"}`} className="nav-link-item">
 										What We Offer
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a href="#why-us" className="nav-link-item">
+									<Link href={`${isContactPage ? "/#why-us" : "#why-us"}`} className="nav-link-item">
 										Why Us
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a href="#fuel-partners" className="nav-link-item">
+									<Link href={`${isContactPage ? "/#fuel-partners" : "#fuel-partners"}`} className="nav-link-item">
 										Fuel Partners
-									</a>
+									</Link>
 								</li>
 							</ul>
 						</nav>
